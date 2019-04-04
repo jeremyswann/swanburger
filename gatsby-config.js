64cb84f442881bdfe621bb12d84a95c9fbc04f87
jeremyswann <mail@jeremyswann.com>
2019-04-03 18:41:24 -0600

    :zap: added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ let contentfulConfig
 
 try {
 	// Load the Contentful config from the .contentful.json
-	contentfulConfig = require('./.contentful')
+	contentfulConfig = require(`./.contentful`)
 } catch (_) {}
 
 // Overwrite the Contentful config with environment variables if they exist
@@ -16,26 +16,25 @@ const { spaceId, accessToken } = contentfulConfig
 
 if (!spaceId || !accessToken) {
 	throw new Error(
-		'Contentful spaceId and the delivery token need to be provided.'
+		`Contentful spaceId and the delivery token need to be provided.`
 	)
 }
 
 module.exports = {
 	siteMetadata: {
-		siteUrl: 'https://swanburger.com',
-		title: 'Swanburger - Freerange Web Development',
-		description:
-			'Freerange web developer (full stack), eCommerce, websites, CMS, ERP, DAM, SaaS and most edge technologies. Farm fresh designs on buns straight out of the oven. Available for consulting and contract work.',
+		siteUrl: `https://swanburger.com`,
+		title: `Swanburger - Freerange Web Development`,
+		description: `Freerange web developer (full stack), eCommerce, websites, CMS, ERP, DAM, SaaS and most edge technologies. Farm fresh designs on buns straight out of the oven. Available for consulting and contract work.`,
 	},
-	pathPrefix: '/gatsby-contentful-starter',
+	pathPrefix: `/gatsby-contentful-starter`,
 	plugins: [
-		'gatsby-transformer-remark',
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-sharp',
-		'gatsby-plugin-sitemap',
+		`gatsby-transformer-remark`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-sharp`,
+		`gatsby-plugin-sitemap`,
 		{
-			resolve: 'gatsby-source-contentful',
+			resolve: `gatsby-source-contentful`,
 			options: contentfulConfig,
 		},
 		{
@@ -46,6 +45,21 @@ module.exports = {
 				// gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
 				// gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
 				// dataLayerName: "YOUR_DATA_LAYER_NAME",
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Swanburger`,
+				short_name: `Swanburger`,
+				start_url: `/`,
+				background_color: `#fafafa`,
+				theme_color: `#008599`,
+				// Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+				// see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+				display: `standalone`,
+				icon: `src/images/icon.png`, // This path is relative to the root of the site.
+				include_favicon: true, // Include favicon
 			},
 		},
 	],
